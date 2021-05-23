f872f0c9792f29b31df84b69b01ccc4af46850d1
GrzegorzL-lab <84191983+GrzegorzL-lab@users.noreply.github.com>
2021-05-23 21:33:34 +0200

    Add files via upload
    day tio hour counter v2, added if...else logic in order to ensure that program doesnt crash easily

--- day_to_hour_counter.py
+++ day_to_hour_counter.py
@@ -5,8 +5,19 @@ name_of_unit = "godzin"
 def days_to_units(num_of_days):
     return f"{num_of_days} dni to {num_of_days * calculation_to_units} {name_of_unit}"
 
-user_input = input("Siema uzytkowniku, wpisz liczbe dni a ja zamienie je na godziny!\n")
-user_input_number = int(user_input)
 
-calculated_value = days_to_units(user_input_number)
-print(calculated_value)
+def validate_and_execute():
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("wpisales 0, wpisz wartosc dodatnia :|")
+
+    else:
+        print("twoj wpis nie jest poprawna wartoscia, prosze nie psuj mi programu :)")
+
+
+user_input = input("Siema uzytkowniku, wpisz liczbe dni a ja zamienie je na godziny!\n")
+validate_and_execute()
