9bc0f568fe0f19fed442ff38a47ebcecb47598aa
GrzegorzL-lab <84191983+GrzegorzL-lab@users.noreply.github.com>
2021-05-24 20:49:53 +0200

    Add files via upload
    try/except, while/for loop, list implemented

--- day_to_hour_counter.py
+++ day_to_hour_counter.py
@@ -7,17 +7,23 @@ def days_to_units(num_of_days):
 
 
 def validate_and_execute():
-    if user_input.isdigit():
-        user_input_number = int(user_input)
+    try:
+        user_input_number = int(num_of_days_element)
+        # chce wykonać zamiane tylko dla dodatnich wartosci
+        # pamietac, że można zachować sobie kod używając znaku #
         if user_input_number > 0:
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
         elif user_input_number == 0:
-            print("wpisales 0, wpisz wartosc dodatnia :|")
-
-    else:
+            print("wpisales 0, wpisz wartosc dodatnia :(")
+        else:
+            print("wpisales ujemna wartosc, wpisz wartosc dodatnia :(")
+    except ValueError:
         print("twoj wpis nie jest poprawna wartoscia, prosze nie psuj mi programu :)")
 
 
-user_input = input("Siema uzytkowniku, wpisz liczbe dni a ja zamienie je na godziny!\n")
-validate_and_execute()
+user_input = ""
+while user_input != "exit":
+    user_input = input("Siema uzytkowniku, wpisz liczbe dni po przecinkach, a ja zamienie je na godziny!\n")
+    for num_of_days_element in user_input.split(", "):
+        validate_and_execute()
